cc87d8831f9290fe9e544443867cf7fbada5c20d
emmadal <emmanueldalougou@gmail.com>
2025-06-07 16:42:42 +0000

    feat: add helmet config

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConsoleLogger, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
-    logger: new ConsoleLogger({
-      json: true,
-      colors: true,
-    }),
   });
+
+  app.use(bodyParser.json());
+  app.use(helmet());
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT ?? 3006, () => {
     console.log(
