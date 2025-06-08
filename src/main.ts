f9c8b249313093b4bcc6112b434030c0ed30161a
emmadal <emmanueldalougou@gmail.com>
2025-06-08 14:19:05 +0000

    refactor: improve security rules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as bodyParser from 'body-parser';
 import helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
 import {
   LoggingInterceptor,
   TimeoutInterceptor,
@@ -11,11 +11,17 @@ import {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: true,
+    bodyParser: true,
+  });
+  app.enableCors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+    methods: 'GET,PUT,POST,DELETE',
+    maxAge: 24 * 60 * 60,
   });
 
-  app.use(bodyParser.json());
   app.use(helmet());
+  app.use(cookieParser());
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
