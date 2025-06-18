d808f7ee9ee8b47fc975a3b00c53abc8887f2611
emmadal <emmanueldalougou@gmail.com>
2025-06-18 05:00:38 +0000

    fix: fix cookie parser import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import {
   LoggingInterceptor,
   TimeoutInterceptor,
